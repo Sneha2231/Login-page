d003de2ce6a5a09cea4d3fa9799009a560142bab
Sneha <snehasinghkumari22@gmail.com>
2025-06-26 16:18:15 +0530

    Add files via upload

---
+++ Login Page/script.js
@@ -0,0 +1,22 @@
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnpopup = document.querySelector('.btnLogin-popup');
+const iconclose = document.querySelector('.icon-close');
+
+
+registerLink.addEventListener('click', ()=> {
+    wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click', ()=> {
+    wrapper.classList.remove('active');
+});
+
+btnpopup.addEventListener('click', ()=> {
+    wrapper.classList.add('active-popup');
+});
+
+icon-close.addEventListener('click', ()=> {
+    wrapper.classList.remove('active-popup');
+});
